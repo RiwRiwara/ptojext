d48d080259824980bff48aa2cf60579cab7e27d2
Riwara <awirut2629@gmail.com>
2025-04-17 20:44:15 +0700

    Disable webpack caching and add prebuild script to clear .next cache

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
